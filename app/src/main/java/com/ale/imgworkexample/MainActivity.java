5a7c6a40a690a8e5f15ee27fd8eb9958d5092a4b
Alessandro <alessandrodev@outlook.com>
2022-02-07 02:11:45 -0300

    commit btn event

--- app/src/main/java/com/ale/imgworkexample/MainActivity.java
+++ app/src/main/java/com/ale/imgworkexample/MainActivity.java
@@ -2,13 +2,30 @@ package com.ale.imgworkexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgSaldo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgSaldo = findViewById(R.id.img_saldo);
+
+        imgSaldo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent chamaSaldo = new Intent(MainActivity.this, Saldo.class);
+                startActivity(chamaSaldo);
+            }
+        });
     }
 }
